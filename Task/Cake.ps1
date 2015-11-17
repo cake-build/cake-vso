97b2b17ef438f3fa825472ee227ac29d124330d4
Patrik Svensson <patrik.svensson@tobii.com>
2015-11-17 23:01:14 +0100

    Fixed issue with restoring NuGet packages.

--- Task/Cake.ps1
+++ Task/Cake.ps1
@@ -5,57 +5,61 @@ Param(
     [string]$arguments
 )
 
-Write-Verbose "Entering script $MyInvocation.MyCommand.Name"
-Write-Verbose "Parameter Values"
+Write-Verbose "Entering script $MyInvocation.MyCommand.Name";
+Write-Verbose "Parameter Values";
 foreach($key in $PSBoundParameters.Keys)
 {
-    Write-Verbose ($key + ' = ' + $PSBoundParameters[$key])
+    Write-Verbose ($key + ' = ' + $PSBoundParameters[$key]);
 }
 
-import-module "Microsoft.TeamFoundation.DistributedTask.Task.Internal"
-import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
+import-module "Microsoft.TeamFoundation.DistributedTask.Task.Internal";
+import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common";
 
 $repositoryPath = $env:BUILD_REPOSITORY_LOCALPATH;
 $toolsPath = Join-Path $repositoryPath "tools";
 $packagePath = Join-Path $toolsPath "packages.config";
 $cakePath = Join-Path $toolsPath "Cake/Cake.exe";
-
-$nuGetPath = Get-ToolPath -Name 'NuGet.exe';
-if (!(Test-Path $nuGetPath)) {
-  Throw "Could not locate NuGet.exe"
-}
-
-# Set the NUGET_EXE environment variable.
-# This is a way of telling Cake where to find nuget.exe.
-$ENV:NUGET_EXE = $nuGetPath;
+$nuGetPath = Join-Path $toolsPath "nuget.exe";
 
 # Check if there's a tools directory.
 if (!(Test-Path $toolsPath)) {
-    Write-Verbose -Message "Creating tools directory..."
+    Write-Host "Creating tools directory...";
     New-Item -Path $toolsPath -Type directory | out-null;
     if (!(Test-Path $toolsPath)) {
-        Throw "Could not create tools directory."
+        Throw "Could not create tools directory.";
     }
 }
 
+# Make sure NuGet exist.
+if (!(Test-Path $nuGetPath)) {
+  # Download NuGet.exe.
+  Write-Verbose "Downloading nuget.exe...";
+  (New-Object System.Net.WebClient).DownloadFile("https://nuget.org/nuget.exe", $nuGetPath);
+  # Make sure it was properly downloaded.
+  if (!(Test-Path $nuGetPath)) {
+      Throw "Could not find nuget.exe";
+  }
+}
+
 # Install prereqs from NuGet.
-Push-Location
-Set-Location $toolsPath
+Push-Location;
+Set-Location $toolsPath;
 if ((Test-Path $packagePath)) {
   # Install tools in packages.config.
-  Invoke-Expression "$nuGetPath install -ExcludeVersion"
+  Write-Host "Restoring packages...";
+  Invoke-Expression "$nuGetPath install `"$packagePath`" -ExcludeVersion -OutputDirectory `"$toolsPath`"";
 }
 if (!(Test-Path $cakePath)) {
   # Install Cake if not part of packages.config.
-  Invoke-Expression "$nuGetPath install Cake -ExcludeVersion"
+  Write-Host "Installing Cake...";
+  Invoke-Expression "$nuGetPath install Cake -ExcludeVersion -OutputDirectory `"$toolsPath`"";
 }
-Pop-Location
+Pop-Location;
 
 # Make sure that Cake has been installed.
 if (!(Test-Path $cakePath)) {
-    Throw "Could not find Cake.exe"
+    Throw "Could not find Cake.exe at $cakePath";
 }
 
 # Start Cake
-Invoke-Expression "$cakePath `"$script`" -target=`"$target`" -verbosity=`"$verbosity`" $arguments"
-exit $LASTEXITCODE
+Invoke-Tool -Path $cakePath -Arguments "`"$script`" -target=`"$target`" -verbosity=`"$verbosity`" $arguments";
